16b6d854cd1c57f2bd71cef5e7c8286b6ce86923
Trung Du <duprogramllc@gmail.com>
2024-10-20 23:47:09 -0500

    fix the aboutus, No ESLint warnings before mess with firebase extraction

--- doctor-finder/app/aboutus/page.tsx
+++ doctor-finder/app/aboutus/page.tsx
@@ -2,17 +2,17 @@ export default function AboutUs() {
     return (
         <div className="container max-w-full">
             {/* Blue background section */}
-            <div className="hero-section" style={{ backgroundColor: '#cbe0f1', paddingBottom: '20px' }}>
+            <div className="hero-section" style={{ backgroundColor: "#cbe0f1", paddingBottom: "20px" }}>
                 <div className="hero-content">
                     <h1 className="text-5xl pt-20 text-background text-center">About Us</h1>
                 </div>
             </div>
 
             {/* Content outside blue background, this format is similar to React dealing with JSX but w/ tailwind in className*/}
-            <div className="OurStory text-black text-5xl text-center" style={{ marginTop: '20px' }}>
+            <div className="OurStory text-black text-5xl text-center" style={{ marginTop: "20px" }}>
                 Our Story
             </div>
-            <div className="AboutUsStory" style={{ marginTop: '20px' }}>
+            <div className="AboutUsStory" style={{ marginTop: "20px" }}>
                 <p>
                     At Code 1, we created a website platform to connect you to qualified doctors tailored to your specific needs. Our user-friendly
                     search engine allows you to easily compare doctors, read and give reviews, and explore their qualifications, ensuring you make informed choices
@@ -20,7 +20,7 @@ export default function AboutUs() {
                     every step of the way. Your health matters to us, and we're committed to helping you find the best care possible. Because at Code1 we care.
                 </p>
             </div>
-            <div className="MeetOurTeam text-black text-5xl font-medium" style={{ marginTop: '40px' }}>
+            <div className="MeetOurTeam text-black text-5xl font-medium" style={{ marginTop: "40px" }}>
                 Meet our team
             </div>
             <div className="OurNames" style={{display:"flex", alignItems: 'center', height: "26vh",}}>
