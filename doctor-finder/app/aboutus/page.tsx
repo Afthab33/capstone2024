a19a8ada7efc22efae13ef71f2897fa9412ff420
marcus <markie@markie.dev>
2024-10-22 00:13:10 -0500

    adjusted padding

--- doctor-finder/app/aboutus/page.tsx
+++ doctor-finder/app/aboutus/page.tsx
@@ -2,7 +2,7 @@ export default function AboutUs() {
     return (
         <div className="container max-w-full">
             <div className="mx-4 sm:mx-8 md:mx-16 lg:mx-20 pb-20">
-                <h1 className="text-5xl pt-20 text-black sm:text-left md:text-center lg:text-center font-semibold">About Us</h1>
+                <h1 className="lg:text-5xl text-4xl pt-10 lg:pt-20 text-black sm:text-left md:text-center lg:text-center font-semibold">About Us</h1>
                 <p className="text-black text-lg sm:text-lg md:text-xl lg:text-xl pt-10 lg:mx-20 xl:mx-60 sm:text-left md:text-center lg:text-center">
                 At Code 1, we created a website platform to connect you to qualified doctors tailored to your specific needs. Our user-friendly search engine allows you to easily compare doctors, read and give reviews, and explore their qualifications, ensuring you make informed choices about your health. With a convenient map lookup, finding a healthcare professional nearby has never been any easier. We are here to support you every step of the way. Your health matters to us, and we're committed to helping you find the best care possible. Because at Code1 we care.
                 </p>
