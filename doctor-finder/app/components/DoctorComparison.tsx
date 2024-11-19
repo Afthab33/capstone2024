46c1b8f63dbe184ba8ca4ceb50a445e09e81059e
laythgha <94503757+laythgha@users.noreply.github.com>
2024-11-19 13:27:23 -0600

    updating DoctorComparison.tsx

--- doctor-finder/app/components/DoctorComparison.tsx
+++ doctor-finder/app/components/DoctorComparison.tsx
@@ -1,1 +1,43 @@
+/*import React from "react";
 
+const DoctorComparison = ({ doctor1, doctor2 }) => {
+  return (
+    <div className="comparison-container">
+      <table className="comparison-table">
+        <thead>
+          <tr>
+            <th>Attribute</th>
+            <th>{doctor1.name}</th>
+            <th>{doctor2.name}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Specialty</td>
+            <td>{doctor1.specialty}</td>
+            <td>{doctor2.specialty}</td>
+          </tr>
+          <tr>
+            <td>Location</td>
+            <td>{doctor1.location}</td>
+            <td>{doctor2.location}</td>
+          </tr>
+          <tr>
+            <td>Insurance Accepted</td>
+            <td>{doctor1.insurance.join(", ")}</td>
+            <td>{doctor2.insurance.join(", ")}</td>
+          </tr>
+          <tr>
+            <td>Languages</td>
+            <td>{doctor1.languages.join(", ")}</td>
+            <td>{doctor2.languages.join(", ")}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DoctorComparison;
+
+*/
