366cc929c80e8ac9dd0c5d477c1fd73acbbf20b1
Daniel Wagner <danielwagner214@gmail.com>
2025-02-01 19:35:16 -0600

    fixed viewDoctor formatting, matches figma

--- doctor-finder/app/viewDoctor/[id]/page.tsx
+++ doctor-finder/app/viewDoctor/[id]/page.tsx
@@ -65,6 +65,19 @@ const ViewDoctor = ({ params }: ViewDoctorProps) => {
     ? acceptedInsurances.length - 4
     : 0;
 
+    const [hovered, setHovered] = useState<string | null>(null);
+    const TwoPeopleSeparated = () => (
+        <div className=' flex item-center'>
+            <User />
+            <EllipsisVertical />
+            <User />
+        </div>
+    );
+
+    const getButtonStyle = (button: string) => ({
+        backgroundColor: hovered === button ? "#E3E4E5" : "#ededed",
+    });
+
   const ReportDoctor = () => (
     <div className='flex items-center' >
         <User />
@@ -86,116 +99,175 @@ const ViewDoctor = ({ params }: ViewDoctorProps) => {
   };
 
   return (
-    <div className="content-center lg:pl-48 md:pl-0 sm:pl-0">
-      <div className="flex flex-row ml-24">
-        <div className="profile-image mb-4 sm:mb-0">
-          <div className="w-24 h-24 bg-gray-200 rounded-full flex items-center justify-center overflow-hidden flex-shrink-0 relative">
-            {previewImage ? (
-              <Image src={previewImage} alt="Profile" fill className="object-cover" />
-            ) : (
-              <div className="w-full h-full bg-gray-200 flex items-center justify-center relative">
-                <Image src="/profpic.png" alt="Profile placeholder" fill className="object-cover" />
-              </div>
-            )}
-          </div>
-        </div>
-        <div className='flex flex-col space-x-6'>
-          <h3 className="flex justify-start text-xl font-semibold text-gray-800 mt-4 ml-6">{displayName}</h3>
-          <h3 className="text-gray-500 text-lg">{specialty}</h3>
-          <div>{streetAddress}, {city}, {state} {zipCode}</div>
-        </div>
-      </div>
-  
-      {/* Adjusting the flex container here */}
-      <div className="flex flex-row gap-8 mt-6">
-        {/* Description Section */}
-        <div className="flex flex-col gap-2 text-sm sm:text-base w-3/4">
-          <div className='flex items-stretch gap-5 rounded-lg' style={{ backgroundColor: "#ededed" }}>
-            <div className="flex items-center gap-2 flex-row">
-              <div className="flex flex-col items-center text-lg font-semibold text-gray-800 ml-4">
-                <span className="text-5xl font-semibold">{rating}</span>                                {/*All ratings right now are 0 in the database, once reviews are finished this will display it*/}
-                <div className='flex flex-row gap-1 items-center'>{fillStars()}</div>
-              </div>
-            </div>
-            <div className='border border-left 5px solid-gray'></div>
-            <div className="flex justify-between items-end flex-col pl-4 pb-4 text-clip overflow-hidden">
-              <div className="text-gray-500 text-sm mb-2 pt-4 pr-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat turpis vitae nisi mollis, a auctor nisl scelerisque.
-              </div>
-              <div className='font-semibold text-sm text-gray-800 pr-4 underline underline-offset-4'>
-                <Link href=''>
-                  See all {reviewCount} reviews
-                </Link>
-              </div>
-            </div>
-          </div>
-  
-          {/* Add other sections like Accepted Insurances or Spoken Languages here */}
-          <div className="flex items-center gap-2 pt-6">
-            <Shield className="w-4 h-4 sm:w-10 sm:h-10 text-blue-500 mb-4" />
-            <div className='flex flex-col'>
-              <span className="text-lg font-semibold text-gray-800">
-                Accepted Insurances
-                {remainingCount > 0 && <span className="font-semibold">+ {remainingCount} more</span>}
-              </span>
-              <h3 className="text-gray-500 text-sm mb-2">{displayInsurances}</h3>
-            </div>
-          </div>
-          <hr className="border-gray-200 mb-2" />
-  
-          <div className="flex items-center gap-2">
-            <MessageCircle className="w-4 h-4 sm:w-10 sm:h-10 text-black-500 mb-4" />
-            <div className='flex flex-col'>
-              <span className="text-lg font-semibold text-gray-800">Spoken Languages</span>
-              <h3 className="text-gray-500 text-sm mb-2">{spokenLanguages.join(', ')}</h3>
-            </div>
-          </div>
-  
-          {/* Report Profile Button */}
-          <div className='flex flex-col'>
-            <hr className="border-gray-200 mb-4" />
-            <div className='flex gap-4'>
-              <div>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <Button className="shadow-md text-gray-400 w-50 h-12">
-                      <div className='flex items-center gap-2 text-gray-400 font-semibold mr-12'>
-                        <ReportDoctor />
-                        Report Profile
-                      </div>
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-[32rem] h-[20rem] relative left-1/4 md:left-3/4">
-                    <div className="grid gap-4">
-                      <div className="space-y-1">
-                        <h4 className="font-medium leading-none">Report Abuse</h4>
-                        <p className="text-sm text-muted-foreground">Click all that apply</p>
-                      </div>
-                      <div className="grid gap-2">
-                        <div className="items-top flex space-x-2">
-                          <Checkbox id="msg1" />
-                          <div className="grid gap-1.5 leading-none">
-                            <label htmlFor="msg1" className="text-sm text-muted-foreground leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                              Multiple account listings
-                            </label>
-                          </div>
+    <div className="flex items-center flex-wrap md:flex-nowrap p-8">
+       <div className="  ">
+                <div className="content-center lg:pl-48 md:pl-0 sm:pl-0">
+                    <div className='flex flex-row ml-24 '>
+                        <div className="profile-image mb-4 sm:mb-0">
+                            <div className="w-24 h-24 bg-gray-200 rounded-full flex items-center justify-center overflow-hidden flex-shrink-0 relative">
+                                {previewImage ? (
+                                    <Image
+                                        src={previewImage}
+                                        alt="Profile"
+                                        fill
+                                        className="object-cover"
+                                    />
+                                ) : (
+                                    <div className="w-full h-full bg-gray-200 flex items-center justify-center relative">
+                                        <Image
+                                            src="/profpic.png"
+                                            alt="Profile placeholder"
+                                            fill
+                                            className="object-cover"
+                                        />
+
+                                    </div>
+                                )}
+                            </div>
                         </div>
-                      </div>
+                        <div className='flex flex-col space-x-6  '>
+                            <h3 className="flex justify-start  text-xl font-semibold text-gray-800 mt-4 ml-6 "> {displayName}</h3>
+                            <h3 className="text-gray-500 text-lg 1px  ">{specialty}</h3>
+                            <div > {streetAddress}, {city}, {state} {zipCode}</div>
+                        </div>
                     </div>
-                  </PopoverContent>
-                </Popover>
-              </div>
+                    <div className="flex flex-col gap-2 text-sm sm:text-base mt-6 mr-20 ml-20 ">
+                        <div className='flex items-stretch gap-5 rounded-lg  '
+                            style={{
+                                backgroundColor: "#ededed"
+                            }}>
+                            <div className="flex items-center gap-2 flex-row">
+                                <div className="flex flex-col items-center  text-lg font-semibold text-gray-800 ml-4">
+                                    <span className="text-5xl font-semibold ">
+                                        {rating}
+                                    </span>
+
+                                    <div className='flex flex-row gap-1 items-center '>
+                                        {fillStars()}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='border border-left 5px solid-gray'> </div>
+                            <div className="flex justify-between items-end flex-col pl-4 pb-4  text-clip overflow-hidden ">
+                                <div className=" text-gray-500 text-sm 1px mb-2 pt-4 pr-2">
+
+                                    Lorem ipsum dolor sit ameue idil fugiat iusto quibusdam nisi exercitationem dolore. Ducimus deleniti sapiente sit quae?
+                                </div>
+                                <div className='font-semibold text-sm text-gray-800 pr-4 underline underline-offset-4'>
+
+                                    <Link href=''>
+                                        See all {reviewCount} reviews
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-2 pt-6">
+                            <Shield className="w-4 h-4 sm:w-10 sm:h-10 text-blue-500 mb-4" />
+                            <div className='flex flex-col '>
+                                <span className="text-lg font-semibold text-gray-800 ">
+                                    Accepted Insurances
+                                    {remainingCount > 0 && <span className="font-semibold">+ {remainingCount} more</span>}
+                                </span>
+                                <h3 className="text-gray-500 text-sm 1px mb-2  ">{displayInsurances}</h3>
+                            </div>
+                        </div>
+                        <hr className="border-gray-200 mb-2" />
+                        <div className="flex items-center gap-2 ">
+                            <MessageCircle className="w-4 h-4 sm:w-10 sm:h-10 text-black-500 mb-4" />
+                            <div className='flex flex-col'>
+                                <span className="text-lg font-semibold text-gray-800">
+                                    Spoken Languages
+                                </span>
+                                <h3 className="text-gray-500 text-sm 1px mb-2  ">{spokenLanguages.join(', ')}</h3>
+                            </div>
+                        </div>
+                        <div className='flex flex-col'>
+                            <hr className="border-gray-200 mb-4" />
+                            <div className='flex gap-4'>
+                                <div>
+                                    <Popover >
+                                        <PopoverTrigger asChild >
+                                            <Button className=" shadow-md text-gray-400 w-50 h-12" style={getButtonStyle("Report Profile")}
+                                                onMouseEnter={() => setHovered("Report Profile")}
+                                                onMouseLeave={() => setHovered(null)}
+                                            >
+                                                <div className='flex items-center gap-2 text-gray-400 font-semibold mr-12 '>
+                                                    <ReportDoctor />
+                                                    Report Profile
+                                                </div>
+                                            </Button>
+                                        </PopoverTrigger >
+                                        <PopoverContent className=" w-[32rem] h-[20rem] relative left-1/4 md:left-3/4 "
+                                        >
+                                            <div className="grid gap-4">
+                                                <div className="space-y-1">
+                                                    <h4 className="font-medium leading-none ">Report Abuse</h4>
+                                                    <p className="text-sm  text-muted-foreground">
+                                                        Click all that apply
+                                                    </p>
+                                                </div>
+                                                <div className="grid gap-2">
+                                                    <div className="items-top flex space-x-2">
+                                                        <Checkbox id="msg1" />
+                                                        <div className="grid gap-1.5 leading-none">
+                                                            <label
+                                                                htmlFor="msg1"
+                                                                className="text-sm  text-muted-foreground leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                                            >
+                                                                Multiple account listings
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                    <div className="items-top flex space-x-2">
+                                                        <Checkbox id="msg2" />
+                                                        <div className="grid gap-1.5 leading-none">
+                                                            <label
+                                                                htmlFor="msg2"
+                                                                className="text-sm text-muted-foreground leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                                            >
+                                                                Invalid doctor information
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                    <div className="items-top flex space-x-2">
+                                                        <Checkbox id="msg3" />
+                                                        <div className="grid gap-1.5 leading-none">
+                                                            <label
+                                                                htmlFor="msg3"
+                                                                className="text-sm text-muted-foreground leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                                            >
+                                                                Doctor did not offer that type of service
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="grid col-span w-full gap-6 ">
+                                                    <Label htmlFor="reason" />
+                                                    <Textarea placeholder="Other reason write here..." id="reason" />
+                                                    <Button>Submit</Button>
+                                                </div>
+                                            </div>
+                                        </PopoverContent>
+                                    </Popover>
+                                </div>
+                                <div>
+                                    <Button className=" shadow-md text-gray-400 w-50 h-12 " style={getButtonStyle("Compare Doctors")}
+                                        onMouseEnter={() => setHovered("Compare Doctors")}
+                                        onMouseLeave={() => setHovered(null)}>
+                                        <div className='flex items-center gap-2 text-gray-400 font-semibold  '>
+                                            <TwoPeopleSeparated />
+                                            Compare Doctors
+                                        </div>
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-  
-        {/* Book Appointment Section (to the right) */}
-        <div className="flex justify-end items-start w-1/4">
-          <div className="mt-6 ml-4">
+          <div>
             <BookAppointment />
           </div>
-        </div>
-      </div>
     </div>
   );
 }  
